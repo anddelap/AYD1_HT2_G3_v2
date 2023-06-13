35f70991fd142c752db7ae2f6a81bee14115bd12
Luis Andrés de la Peña Pineda <3007737490101@gmail.com>
2023-06-13 00:43:11 -0600

    [add]primo

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"strconv"
 
@@ -18,6 +19,9 @@ import (
 type Operacion struct {
 	Left  int `json:"left"`
 	Right int `json:"right"`
+}
+type Numero struct {
+	Value int `json:"value"`
 }
 
 func suma(w http.ResponseWriter, r *http.Request) {
@@ -36,9 +40,34 @@ func resta(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, result)
 }
 
+func primo(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var numero Numero
+	json.Unmarshal(reqBody, &numero)
+	var result string
+	if numero.Value < 2 {
+		result = strconv.Itoa(numero.Value) + " no es primo"
+	} else {
+		limite := int(math.Sqrt(float64(numero.Value)))
+
+		for i := 2; i <= limite; i++ {
+			if numero.Value%i == 0 {
+				result = strconv.Itoa(numero.Value) + " no es primo"
+				break
+			} else if numero.Value%i != 0 {
+				result = strconv.Itoa(numero.Value) + " es primo"
+				break
+			}
+		}
+	}
+	fmt.Fprintf(w, result)
+}
+
 func main() {
 	fmt.Println("Api corriendo en 'http://localhost:8000")
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/suma", suma).Methods("POST")
+	router.HandleFunc("/resta", resta).Methods("POST")
+	router.HandleFunc("/primo", primo).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
